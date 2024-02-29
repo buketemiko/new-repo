3315c6ba04d0b9679543183420f8f6f724a7db59
Frank Lorna <161691320+buketemiko@users.noreply.github.com>
2024-02-29 09:22:01 +0200

    Create code.cs

---
+++ code.cs
@@ -0,0 +1,22 @@
+class CachedValue<T> where T : class
+	{
+		Func<T> CalculateProc;
+		T LastCalculatedValue;
+
+		public bool IsFrozen { get; private set; }
+		public void Freeze () => IsFrozen = true;
+
+		public CachedValue (Func<T> calculateProc)
+		{
+			CalculateProc = calculateProc;
+		}
+
+		public T Value {
+			get {
+				if (IsFrozen) {
+					if (LastCalculatedValue == null)
+						LastCalculatedValue = CalculateProc ();
+					return LastCalculatedValue;
+				} else {
+					return CalculateProc ();
+				}
